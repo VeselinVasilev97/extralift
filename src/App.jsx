f435349f477955d8fe721a4f167790374909642f
Dus7a <134999303+Dus7a@users.noreply.github.com>
2023-08-12 14:53:25 +0300

    extralift start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Nav from './components/Nav/Nav'
+import classes from './App.module.css'
+
+function App() {
+
+  return (
+    <>
+      <div className={classes.architecture}>
+        <Nav />
+        <Nav />
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,15 @@
+import React,{useEffect,useState} from "react";
+import classes from './Nav.module.css';
+
+const Nav = () => {
+
+
+    return(
+        <div className={classes.NavMainDiv}>
+            
+        </div>
+    )
+}
+
+
+export default Nav
